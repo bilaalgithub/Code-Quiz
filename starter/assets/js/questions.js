afd2790c490bb2d63a8d2dffebc1ce5057cdf9ee
bilaalgithub <bilaalkulane13@gmail.com>
2023-01-20 19:38:59 +0000

    added 10 questions and answers

---
+++ starter/assets/js/questions.js
@@ -0,0 +1,58 @@
+// list of all questions, choices and answers.
+
+var questions = [
+    {
+        title: "Javascript is an _______ language?",
+        choices: ["Object-Oriented", "Object-Based", "Procedural", "None of the above"],
+        answer: "Object-Oriented"
+    },
+    {
+        title: "Which of the following keywords is used to define a variable in Javascript?",
+        choices:["var","let","Both A and B","None of the above"],
+        answer:"Both A and B",
+    },
+    {
+        title:"Which of the following methods is used to access HTML elements using Javascript?",
+        choices:["getElementbyId()", "getElementsByClassName()","Both A and B", "None of the above"],
+        answer:"Both A and B",
+    },
+    {
+        title:"Upon encountering empty statements, what does the Javascript Interpreter do?",
+        choices:["Throws an error", "Ignores the statement","Gives a warning", "None of the above"],
+        answer:"Ignores the statement",
+    },
+    {
+        title:"Which of the following methods can be used to display data in some form using Javascript?",
+        choices:["document.write()", "console.log()","window.alert()", "All of the above"],
+        answer:"All of the above",
+    },
+    {
+        title:"How can a datatype be declared to be a constant type?",
+        choices:["const", "var","let", "constant"],
+        answer:"const",
+    },
+    {
+        title:"What keyword is used to check whether a given property is valid or not?",
+        choices:["in", "is in","exists", "lies"],
+        answer:"in",
+    },
+    {
+        title:"What is the use of the <noscript> tag in Javascript?",
+        choices:["The contents are displayed by non-JS-based browsers", "Clears all the cookies and cache","Both A and B", "None of the above"],
+        answer:"The contents are displayed by non-JS-based browsers",
+    },
+    {
+        title:"When an operators value is NULL, the typeof returned by the unary operator is:",
+        choices:["Boolean", "Undefined","Object", "Integer"],
+        answer:"Object",
+    },
+    {
+        title:"What does the Javascript “debugger” statement do?",
+        choices:["It will debug all the erros in the program at runtime", "It acts as a breakpoint in a program","It will debug error in the current statement if any", "All of the above"],
+        answer:"It acts as a breakpoint in a program",
+    },
+]
+
+
+
+
